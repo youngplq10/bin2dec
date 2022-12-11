efac3527aa8c231b29b08950204eadc4cf74b8b9
starzynski5_ <patryk.plq@o2.pl>
2022-12-11 16:40:51 +0100

    Convert binary to decimal app
    React & React-DOM ^18.2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    //Input changes are being saved to variable
+    const [change, setChange] = useState(0)
+    const handleChange = (changes) => {
+        setChange(changes.target.value)
+    }
+
+    //Convert binary to decimal
+    const convertToDecimal = () => {
+        let input = change.split("")
+        input.reverse()
+        let power = 1;
+        let result = 0;
+        for(let i=0; i<=input.length; i++){
+            if(input[i]==="1"){
+                result += power
+                power*=2
+            }else{
+                power*=2
+            }
+        }
+        document.getElementById('result').innerHTML = '<span>'+result+'</span>'
+    }
+
+    //Check if every digit is 0 or 1
+    const inputCheck = (event) => {
+        event.preventDefault()
+        for (let i = 0; i < change.length; i++) {
+            if (change[i] !== '0' && change[i] !== '1') {
+                document.getElementById('result').innerHTML = '<span style="color: red">error: input can only handle 1s and 0s</span>';
+                break
+            } else {
+                document.getElementById('result').innerHTML = '';
+                convertToDecimal()
+            }
+        }
+    }
+    //Return view of app
+    return (
+        <div>
+            <div className='input-class'>
+                <form onSubmit={inputCheck}>
+                    <label htmlFor='binary-input'>Input binary</label><br/>
+                    <input type='text' id='binary-input' onChange={handleChange}/><br/>
+                    <input type='submit' value='Convert'  />
+                </form>
+                <p id='result'></p>
+            </div>
+        </div>
+      )
+    }
 
 export default App;
